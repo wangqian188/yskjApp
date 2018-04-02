ed6ddbbe2e1081b0b86438f6fdd0c300af8cde16
yuan <zylucying@163.com>
2018-04-02 15:50:23 +0800

    2018-04-02 张渊

--- js/lib/sy.js
+++ js/lib/sy.js
@@ -1,9 +1,13 @@
 //检测网络环境是否链接
 function networkLink(){
 	if (plus.networkinfo.getCurrentType() == plus.networkinfo.CONNECTION_NONE) {
-		mui.toast("网络异常，请检查网络设置！");
-	}else{
-		window.onload();
+		mui.alert("网络异常，请检查网络设置！", function(){
+//			alert(123);
+		});
+//		mui.toast("网络异常，请检查网络设置！");
 	}
+//	else{
+//		window.onload();
+//	}
 }
 
