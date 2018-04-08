628a0d4e11e11e9d83f53983bc450488288566ed
yuan <zylucying@163.com>
2018-04-08 09:20:37 +0800

    提交2018-04-08 张渊 09:20

--- js/lib/sy.js
+++ js/lib/sy.js
@@ -1,13 +1,29 @@
 //检测网络环境是否链接
 function networkLink(){
 	if (plus.networkinfo.getCurrentType() == plus.networkinfo.CONNECTION_NONE) {
-		mui.alert("网络异常，请检查网络设置！", function(){
+//		mui.alert("网络异常，请检查网络设置！", function(){
 //			alert(123);
-		});
-//		mui.toast("网络异常，请检查网络设置！");
-	}
+//		});
+		mui.toast("网络异常，请检查网络设置！");
+}
 //	else{
 //		window.onload();
 //	}
 }
+//获取版本号
+//mui.plusReady(function(){
+//	plus.runtime.getProperty(plus.runtime.appid,function(inf){
+//		alert(inf.version);
+//		return inf.version;
+//	});	
+//	plusReady();
+//});
+//
+//var wgtVer=null; 
+//function plusReady(){ // 获取本地应用资源版本号 
+//  plus.runtime.getProperty(plus.runtime.appid,function(inf){ 
+//      wgtVer=inf.version; 
+//      alert(wgtVer);
+//  }); 
+//} 
 
