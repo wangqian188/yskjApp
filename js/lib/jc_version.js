65e286b3ffb2b9cfe22918dc75d1d56cde570fac
yuan <zylucying@163.com>
2018-05-30 10:33:24 +0800

    2018-05-30 张渊 10:33

--- js/lib/jc_version.js
+++ js/lib/jc_version.js
@@ -4,6 +4,24 @@ var u = navigator.userAgent, app = navigator.appVersion;
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
 var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 var appUrl="";
+//版本对比方法
+var versionfunegt = function (ver1,ver2) {
+    var version1pre = parseFloat(ver1);
+    var version2pre = parseFloat(ver2);
+    var version1next =  ver1.replace(version1pre + ".","");
+    var version2next =  ver2.replace(version2pre + ".","");
+    if(version1pre > version2pre){
+        return true;
+    }else if(version1pre < version2pre){
+        return false;
+    }else{
+        if(version1next >= version2next){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
 function version(){
   	$.ajax({  
           type:'post',  
@@ -17,9 +35,10 @@ function version(){
 			       mui.plusReady(function(){
 			       		//获取本地版本号
 						plus.runtime.getProperty(plus.runtime.appid,function(inf){ 
-					        var wgtVer=inf.version; 
-					       if(data.data.versionCode != wgtVer){
-						        var btnArray = ['忽略更新', '立即更新'];
+					        var wgtVer = inf.version; //旧版本(本地)
+				        	var newwgVer = data.data.versionCode;//新版本
+				        	if(wgtVer != newwgVer && versionfunegt(newwgVer,wgtVer)){
+				        		var btnArray = ['忽略更新', '立即更新'];
 								mui.confirm('您当前的版本为'+wgtVer+'，发现最新版本'+data.data.versionCode+'，是否下载更新？', '提示', btnArray, function(e) {
 								 	if (e.index == 0) {
 								 		mui.toast("已取消更新！");
@@ -47,10 +66,10 @@ function version(){
 					                    dd.start();
 								 	}
 								},'div')
-							       }else{
-							       		mui.toast("已经最新版本！");
-							       		return;
-							       }
+				        	}else{
+					       		mui.toast("已经最新版本！");
+					       		return;
+					        }
 							    });	
 							});
 					    }else if (isIOS) {
